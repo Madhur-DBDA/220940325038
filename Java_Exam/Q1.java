fe0360fb56228d94e486b5d63446367f786b58fc
Madhur-DBDA <118419161+Madhur-DBDA@users.noreply.github.com>
2022-11-17 17:07:23 +0530

    Create Q1.java

---
+++ Java_Exam/Q1.java
@@ -0,0 +1,16 @@
+class Q1
+{
+    public static void main(String[] args)
+    {
+        String[] arr = new String[5];
+        arr[0] = "First String";
+        arr[1] = "Mango";
+        arr[2] = "Peach";
+        arr[3] = "Fourth String";
+        arr[4] = "Fifth String";
+        for(String i: arr)
+        {
+            System.out.println(i);
+        }
+    }
+}
